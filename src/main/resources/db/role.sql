7eb2681fb609ec337ef88b81cc162cd08e1a0532
dfunme <happyleaves@163.com>
2017-09-05 14:41:28 +0800

    update

--- src/main/resources/db/role.sql
+++ src/main/resources/db/role.sql
@@ -4,9 +4,9 @@
   #end
   
   #sql("list")
-    select * from sys_role r where status = 1
+    select * from sys_role r where r.status = 1
     #if(user_id != null)
-      and exists (select 1 from sys_authority a where a.role_id = r.id and a.user_id = #(user_id))
+      and exists (select 1 from sys_user_role ua where ua.role_id = r.id and ua.user_id = #para(user_id))
     #end
   #end
 #end
